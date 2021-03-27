f28b32dba97fb83b605f6d45d74ed364dceae653
Duncan <reallyconnected@gmail.com>
2021-03-27 15:51:10 +0000

    Adding a state variable and Pixel/Circle/Rectangle plotting options. PCR keys.

--- raylib_fire/src/main.rs
+++ raylib_fire/src/main.rs
@@ -13,18 +13,33 @@ const WINDOW_HEIGHT: usize = 2160;
 const WINDOW_FACTOR: usize = 3;
 const RECTANGLE_GRID_WIDTH: i32 = 0;
 
-fn handle_key(rl: &mut RaylibHandle) {
-    // if rl.is_key_pressed(KEY_P) {
-    //     all_stars.set_draw_type(stars3d::DrawType::Pixel);
-    // }
+pub enum DrawType {
+    Pixel,
+    Rectangle,
+    Circle,
+}
 
-    // if rl.is_key_pressed(KEY_C) {
-    //     all_stars.set_draw_type(stars3d::DrawType::Circle);
-    // }
+struct StateStore {
+    draw_type: DrawType,
+}
+impl StateStore {
+    pub fn new(init_draw_type: DrawType) -> StateStore {
+        StateStore { draw_type: init_draw_type }
+    }
+}
 
-    // if rl.is_key_pressed(KEY_R) {
-    //     all_stars.set_draw_type(stars3d::DrawType::Rectangle);
-    // }
+fn handle_key(rl: &mut RaylibHandle, state: &mut StateStore) {
+    if rl.is_key_pressed(KEY_P) {
+        state.draw_type = DrawType::Pixel;
+    }
+
+    if rl.is_key_pressed(KEY_C) {
+        state.draw_type = DrawType::Circle;
+    }
+
+    if rl.is_key_pressed(KEY_R) {
+        state.draw_type = DrawType::Rectangle;
+    }
 
     // if rl.is_key_down(KEY_LEFT_ALT) && rl.is_key_pressed(KEY_PAGE_UP) {
     //     all_stars.adjust_star_number(500);
@@ -42,9 +57,9 @@ fn handle_key(rl: &mut RaylibHandle) {
     //     all_stars.adjust_star_number(-100);
     // }
 
-     if rl.is_key_down(KEY_LEFT_ALT) && rl.is_key_pressed(KEY_ENTER) {
-         rl.toggle_fullscreen();
-     }
+    if rl.is_key_down(KEY_LEFT_ALT) && rl.is_key_pressed(KEY_ENTER) {
+        rl.toggle_fullscreen();
+    }
 
     // if rl.is_key_pressed(KEY_RIGHT_BRACKET){
     //     all_stars.increase_focal_length();
@@ -60,7 +75,6 @@ fn print_type_of<T>(_: &T) {
 }
 
 fn main() {
-
     let mut fire_manager = fire::FireManager::new(fire::FIRE_GRID_WIDTH, fire::FIRE_GRID_HEIGHT);
 
     let mut load_image_result = Image::load_image(r"C:\Duncan\source\rust\rusty_stuff\raylib_fire\target\debug\white_yellow_orange_black_black_512.png");
@@ -96,17 +110,17 @@ fn main() {
 
     let mut frame_counter = 0;
 
+    let mut state: StateStore = StateStore::new(DrawType::Rectangle);
+
     while !rl.window_should_close() {
-        handle_key(&mut rl);
+        handle_key(&mut rl, &mut state);
         frame_counter += 1;
 
         let mut d = rl.begin_drawing(&thread);
         let fps = d.get_fps();
         let fps_string = format!("FPS: {}", fps,);
 
-
-        if frame_counter % FRAMES_TO_ADD_GAS == 0
-        {
+        if frame_counter % FRAMES_TO_ADD_GAS == 0 {
             fire_manager.add_gas();
         }
         fire_manager.animate();
@@ -116,32 +130,43 @@ fn main() {
         let screen_width = d.get_screen_width();
         let screen_height = d.get_screen_height();
 
-        let rectangle_width = screen_width / fire_manager.grid_width_as_i32() ;
+        let rectangle_width = screen_width / fire_manager.grid_width_as_i32();
         let rectangle_height = screen_height / fire_manager.grid_height_as_i32();
         let rectangles_in_a_row = (screen_width / rectangle_width) as usize;
         let rectangle_row_max = (screen_height / rectangle_height) as usize;
 
         let mut out_rectangle_y = 0 as usize;
         let mut rectangle_location_y = 0;
-        while out_rectangle_y < rectangle_row_max -2  {
+        while out_rectangle_y < rectangle_row_max - 2 {
             let mut out_rectangle_x = 0 as usize;
             let mut rectangle_location_x = 0;
             while out_rectangle_x < rectangles_in_a_row {
-                d.draw_rectangle(
-                    rectangle_location_x + RECTANGLE_GRID_WIDTH,
-                    rectangle_location_y + RECTANGLE_GRID_WIDTH,
-                    rectangle_width - RECTANGLE_GRID_WIDTH ,
-                    rectangle_height - RECTANGLE_GRID_WIDTH,
-                    fire_manager.get_fire_colour_value(out_rectangle_x, out_rectangle_y));
-                // d.draw_pixel(rectangle_location_x + (rectangle_width / 2),
-                //              rectangle_location_y + (rectangle_height / 2),
-                //              fire_manager.get_fire_colour_value(out_rectangle_x, out_rectangle_y));
-                // d.draw_circle(rectangle_location_x + (rectangle_width / 2),
-                //               rectangle_location_y + (rectangle_height / 2),
-                //               (rectangle_height >> 2) as f32,
-                //               fire_manager.get_fire_colour_value(out_rectangle_x, out_rectangle_y));
-
-
+                match state.draw_type {
+                    DrawType::Rectangle => {
+                        d.draw_rectangle(
+                            rectangle_location_x + RECTANGLE_GRID_WIDTH,
+                            rectangle_location_y + RECTANGLE_GRID_WIDTH,
+                            rectangle_width - RECTANGLE_GRID_WIDTH,
+                            rectangle_height - RECTANGLE_GRID_WIDTH,
+                            fire_manager.get_fire_colour_value(out_rectangle_x, out_rectangle_y),
+                        );
+                    }
+                    DrawType::Pixel => {
+                        d.draw_pixel(
+                            rectangle_location_x + (rectangle_width / 2),
+                            rectangle_location_y + (rectangle_height / 2),
+                            fire_manager.get_fire_colour_value(out_rectangle_x, out_rectangle_y),
+                        );
+                    }
+                    DrawType::Circle => {
+                        d.draw_circle(
+                            rectangle_location_x + (rectangle_width / 2),
+                            rectangle_location_y + (rectangle_height / 2),
+                            (rectangle_height >> 2) as f32,
+                            fire_manager.get_fire_colour_value(out_rectangle_x, out_rectangle_y),
+                        );
+                    }
+                }
 
                 out_rectangle_x += 1;
                 rectangle_location_x += rectangle_width;
